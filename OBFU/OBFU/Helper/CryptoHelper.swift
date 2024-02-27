c95e9878a00500a82a23978b2b4881e1db00028b
Aaron Yen <yen064@gmail.com>
2024-02-27 15:50:58 +0800

    Try some f**kin sh*t.
    (cherry picked from commit add744ba2ae69cc18e6311d709df54cbc6f90eba)

--- OBFU/OBFU/Helper/CryptoHelper.swift
+++ OBFU/OBFU/Helper/CryptoHelper.swift
@@ -4,10 +4,24 @@
 //
 //  Created by Aaron Yen on 2024/2/26.
 //
-
-import Foundation
 import CryptoSwift
 
 class CryptoHelper {
-    
+    static
+    func test() -> Void {
+        _ = KeyGenerator(seed: "2.36.1")
+    }
+//    static fileprivate func AESKeyGenerator() -> String {
+//
+//    }
+}
+
+fileprivate struct KeyGenerator {
+    let prefix: String = "obfuscator"
+    let suffix: String = "obfuscator"
+    init(seed: String) {
+        let str96 = String(format: "%@%@%@", prefix.md5(), seed.md5(), suffix.md5())
+        print(str96)
+        print(seed)
+    }
 }
