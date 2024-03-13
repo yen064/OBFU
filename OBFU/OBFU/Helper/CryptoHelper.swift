4acc8f53b1f5039dfb49ac2f4ca9d521f720407c
Aaron Yen <yen064@gmail.com>
2024-03-14 03:50:31 +0800

    adjust more in CryptoHelper
    (cherry picked from commit a5aab7e50210b4e411ae5c854334edc1bdff701e)

--- OBFU/OBFU/Helper/CryptoHelper.swift
+++ OBFU/OBFU/Helper/CryptoHelper.swift
@@ -8,23 +8,38 @@ import CryptoSwift
 
 class CryptoHelper {
     static func test() -> Void {
-        let kg = KeyGenerator(seed: "2.36.1")
-        print("aesKey => \(kg.aesKeyStr)")
-        print("aesIv => \(kg.aesIvStr)")
+        cryptoTest(keyGen: KeyGenerator())
+        cryptoTest(keyGen: KeyGenerator(seed: "2.36.1"))
+    }
+    
+    fileprivate static func cryptoTest(keyGen: KeyGenerator) -> Void {
+        let rawStr = "loginInfoModel"
+        let encryptedStr: String? = CryptoHelper(keyGen).encrypt(rawStr)
+        var decryptedStr: String?
         
-        let a: String? = CryptoHelper(kg).encrypt("loginInfoModel")
-        if let aStr = a {
-            print(aStr)
+        if let encryptedString = encryptedStr {
+            decryptedStr = CryptoHelper(keyGen).decrypt(encryptedString)
         }
         
+        print("")
+        print("加密測試:")
+        print("aesKey => \(keyGen.aesKeyStr)")
+        print("aesIv => \(keyGen.aesIvStr)")
+        
+        print("rawStr = \(rawStr)")
+        print("encryptedStr = \(encryptedStr ?? "")")
+        print("decryptedStr = \(decryptedStr ?? "")")
+        print("")
     }
+    
     fileprivate static var defaultSeed: String {
         return "1.0.0"
     }
     fileprivate var keyGenerator: KeyGenerator = KeyGenerator(seed: CryptoHelper.defaultSeed)
+    
     init(_ keyGenerator: KeyGenerator? = nil) {
-        if let kg = keyGenerator {
-            self.keyGenerator = kg
+        if let keyGen = keyGenerator {
+            self.keyGenerator = keyGen
         }
     }
     func encrypt(_ source: String) -> String? {
@@ -35,14 +50,6 @@ class CryptoHelper {
     }
     fileprivate func encrypt(source: String) throws -> String {
         do {
-            if let aesKeyBytes = keyGenerator.aesKeyStr.data(using: .utf8) {
-                print("aesKeyBytes.count = \(aesKeyBytes.count)")
-                
-            }
-            if let aesIvBytes = keyGenerator.aesIvStr.data(using: .utf8) {
-                print("aesIvBytes.count = \(aesIvBytes.count)")
-                
-            }
             let aes = try AES(key: keyGenerator.aesKeyStr, iv: keyGenerator.aesIvStr)
             let encryptBytes = try aes.encrypt(Array(source.utf8))
             let encryptStr = encryptBytes.toBase64()
@@ -51,13 +58,26 @@ class CryptoHelper {
             throw error
         }
     }
-//    static fileprivate func AESKeyGenerator() -> String {
-//
-//    }
+    
+    func decrypt(_ source: String) -> String? {
+        if let decryptStr = try? decrypt(source: source) {
+            return decryptStr
+        }
+        return nil
+    }
+    fileprivate func decrypt(source: String) throws -> String {
+        do {
+            let aes = try AES(key: keyGenerator.aesKeyStr, iv: keyGenerator.aesIvStr)
+            let decryptedStr = try source.decryptBase64ToString(cipher: aes)
+            return decryptedStr
+        } catch let error {
+            throw error
+        }
+    }
 }
-
+// MARK: -
 struct KeyGenerator {
-    private var sourceSeed: String = "N/A"
+    private var sourceSeed: String = CryptoHelper.defaultSeed
     var aesKeyStr: String {
         let charArr: [String] = self.sourceSeed.md5().map { String($0) }
         var newCharArr: [String] = []
@@ -80,7 +100,9 @@ struct KeyGenerator {
         let ivStr = newCharArr.joined()
         return ivStr
     }
-    init(seed: String) {
-        self.sourceSeed = seed
+    init(seed: String? = nil) {
+        if let seedStr = seed {
+            self.sourceSeed = seedStr
+        }
     }
 }
